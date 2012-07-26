c662aef2918788c9de04070f4c7ebc4182f20932
pvdvreede <paul@vdvreede.net>
2012-07-26 16:38:19 +1000

    Fix formatting.
    remove tabs and replace with indent of 2 spaces.

--- lib/project_patch.rb
+++ lib/project_patch.rb
@@ -1,32 +1,32 @@
 # patch Project model so that it will add a user or group as members on creation
 
 module DefaultProjectMembers
-	module ProjectPatch
-		def self.included(base)
-			base.extend(ClassMethods)
-			base.send(:include, InstanceMethods)
-			base.class_eval do 
-				unloadable
-				
-				# add filter for after a project is created
-				after_create :add_default_members_to_project
-			end
-		end
+  module ProjectPatch
+    def self.included(base)
+      base.extend(ClassMethods)
+      base.send(:include, InstanceMethods)
+      base.class_eval do 
+        unloadable
 
-		module ClassMethods
-		end
+        # add filter for after a project is created
+        after_create :add_default_members_to_project
+      end
+    end
 
-		module InstanceMethods
-			# filter method to add a member to the project after saving it
-			def add_default_members_to_project
-				default_members = DefaultProjectMember.all
-				default_members.each do |m|
-					member = Member.new( :role_ids => [m.role_id], :user_id => m.user_id, :project_id => self.id )			
-					self.members << member
-				end
-			end
-		end
-	end
+    module ClassMethods
+    end
+
+    module InstanceMethods
+      # filter method to add a member to the project after saving it
+      def add_default_members_to_project
+        default_members = DefaultProjectMember.all
+        default_members.each do |m|
+          member = Member.new( :role_ids => [m.role_id], :user_id => m.user_id, :project_id => self.id )			
+          self.members << member
+        end
+      end
+    end
+  end
 end
 
 Project.send(:include, DefaultProjectMembers::ProjectPatch)
